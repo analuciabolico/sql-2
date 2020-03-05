e6b2349c6af5267e646ab0af7fe679e823fb9f8a
anakettl <anakettl@gmail.com>
2020-03-04 22:08:32 -0300

    Adicionado script de exclusao para tabela area

--- aula-02-03-2020/drop/drop.sql
+++ aula-02-03-2020/drop/drop.sql
@@ -1,3 +1,4 @@
+drop table area cascade constraints;
 drop table curso cascade constraints;
 drop table disciplina cascade constraints;
 drop table aluno cascade constraints;
@@ -6,6 +7,7 @@ drop table turma cascade constraints;
 drop table turma_aluno cascade constraints;
 drop table aluno_curso cascade constraints;
 
+drop sequence s_id_area;
 drop sequence s_id_curso;
 drop sequence s_id_disciplina;
 drop sequence s_id_aluno;
